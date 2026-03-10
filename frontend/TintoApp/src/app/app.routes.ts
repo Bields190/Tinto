6112030ab7236df0df41f19b5665991901b67793
Khalil Brito <149338575+khalildebrito@users.noreply.github.com>
2026-03-09 22:27:57 -0500

    Feat: Tela de cadastro meio bixada talvez eu ajeite depois kkkkkk

--- frontend/TintoApp/src/app/app.routes.ts
+++ frontend/TintoApp/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from './component/landing-page/landing-page';
+import { Cadastro } from './component/cadastro/cadastro';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
-  { path: 'inicio', component: LandingPage }
+  { path: 'inicio', component: LandingPage },
+  { path: 'cadastro', component: Cadastro }
 ];
---
+++ frontend/TintoApp/src/app/component/cadastro/cadastro.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [CommonModule],
+  templateUrl: './cadastro.html',
+  styleUrl: './cadastro.scss',
+})
+export class Cadastro {}
