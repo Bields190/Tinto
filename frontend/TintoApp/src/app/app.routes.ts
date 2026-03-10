c70574eeeac0ca6695d427ac72dec7afd6e825fb
Gabs <gabriel.daniel@sou.ufac.br>
2026-03-09 21:39:25 -0500

    FEAT: Página de Login

--- frontend/TintoApp/src/app/app.routes.ts
+++ frontend/TintoApp/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from './component/landing-page/landing-page';
+//import { Cadastro } from './component/cadastro/cadastro';
+import { Login } from './component/login/login';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
-  { path: 'inicio', component: LandingPage }
+  { path: 'inicio', component: LandingPage },
+  // { path: 'cadastro', component: Cadastro },
+  { path: 'login', component: Login }
 ];
