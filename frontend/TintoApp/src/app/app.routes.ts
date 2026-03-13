19431b5f24d268cdcc72f2ea49fcd5b2a8a23ae5
Khalil Brito <149338575+khalildebrito@users.noreply.github.com>
2026-03-13 00:28:19 -0500

    feat: tela de adicionar/editar vinho

--- frontend/TintoApp/src/app/app.routes.ts
+++ frontend/TintoApp/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LandingPage } from './component/landing-page/landing-page';
 import { Cadastro } from './component/cadastro/cadastro';
 import { Login } from './component/login/login';
 import { Perfil } from './component/perfil/perfil';
+import { Adicionar } from './component/adicionar/adicionar';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: 'inicio', component: LandingPage },
   { path: 'cadastro', component: Cadastro },
   { path: 'login', component: Login },
-  { path: 'perfil', component: Perfil }
+  { path: 'perfil', component: Perfil },
+  { path: 'adicionar', component: Adicionar }
 ];
